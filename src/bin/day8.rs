c058d45c3e76bdd581cf60a4b32b21def9a0c3fb
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-16 16:49:58 +0200

    day 8 part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -5,6 +5,9 @@ fn main() {
 
 	let part1 = file.lines().map(|x| x.len() - str_val(x)).sum::<usize>();
 	println!("Day 8 part 1: {}", part1);
+
+	let part2 = file.lines().map(encode).map(|x| x.len() - str_val(&x)).sum::<usize>();
+	println!("Day 8 part 2: {}", part2);
 }
 
 fn str_val(x: &str) -> usize {
@@ -21,4 +24,20 @@ fn str_val(x: &str) -> usize {
 		count += 1;
 	}
 	count - 2
+}
+
+fn encode(x: &str) -> String {
+	let mut str = String::new();
+	str.push_str("\"");
+	for c in x.chars() {
+		if c == '"' {
+			str.push_str("\\\"");
+		} else if c == '\\' {
+			str.push_str("\\\\");
+		} else {
+			str.push(c);
+		}
+	}
+	str.push_str("\"");
+	str
 }
