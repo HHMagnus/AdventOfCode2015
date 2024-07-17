a3366343fcc9565c7a845619c97ef4b26c98a2b1
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-17 09:52:39 +0200

    day 19 unoptimized

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs::read_to_string};
+use std::{collections::{HashSet, VecDeque}, fs::read_to_string};
 
 fn main() {
 	let file = read_to_string("input/day19.txt").unwrap();
@@ -10,11 +10,32 @@ fn main() {
 		(split.next().unwrap(), split.next().unwrap())
 	}).collect::<Vec<_>>();
 
-	let medicine = split.next().unwrap();
+	let medicine = split.next().unwrap().lines().next().unwrap();
 
-	let part1 = transforms.into_iter().flat_map(|x| apply(medicine, x)).collect::<HashSet<_>>();
+	let part1 = transforms.iter().flat_map(|&x| apply(medicine, x)).collect::<HashSet<_>>();
 
 	println!("Day 19 part 1: {}", part1.len());
+
+	let mut queue = VecDeque::new();
+	queue.push_back((medicine.to_string(), 1));
+	let mut visited = HashSet::new();
+	visited.insert(medicine.to_string());
+
+	'bfs: while let Some((str, steps)) = queue.pop_front() {
+
+		let x = transforms.iter().flat_map(|&(t1, t2)| apply(&str, (t2, t1))).min_by_key(|x| x.len()).unwrap();
+				if visited.contains(&x) {
+					continue;
+				}
+				visited.insert(x.clone());
+
+				if x == "e" {
+					println!("Day 19 part 2: {}", steps);
+					break 'bfs;
+				}
+
+				queue.push_front((x, steps + 1));
+	}
 }
 
 fn apply(medicine: &str, transform: (&str, &str)) -> Vec<String> {
